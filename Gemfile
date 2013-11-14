40d25a83f01549a1ac0b2f7f0e6a4a714f18f366
Justin Lambert <justin.lambert@bigcommerce.com>
2013-11-14 13:41:26 +1100

    WIP on xml templating options

--- Gemfile
+++ Gemfile
@@ -1,10 +1,12 @@
-ruby '2.0.0'
-
 source 'https://rubygems.org'
-
+ruby '1.9.3'
 
 gem "resque", "~> 2.0.0.pre.1", github: "resque/resque"
 gem "bigcommerce", github: "bigcommerce/bigcommerce-api-ruby"
-
-
+gem "redis", "~> 3.0.1"
+gem "hiredis", "~> 0.4.5"
+gem "em-synchrony"
+gem "ruby-xslt", "~> 0.9.9"
+gem "nokogiri"
 gem 'pry'
+gem "active_support"
--- main.rb
+++ main.rb
@@ -1,9 +1,15 @@
 require 'bigcommerce'
 require 'pry'
+require './grab_data'
+require './export_data'
 
 api = Bigcommerce::Api.new({
     :store_url => "https://store-gt1zgky4.bcapp.dev",
     :username  => "admin",
     :api_key   => "1320fd79b5b47c8173d642e33879002ffb1c7ff2"
 })
-binding.pry
+
+import = GrabData.new(api)
+# import.work
+export = ExportData.new("product")
+export.work
