13794807ac9f6e31c78d7f7ffd7f16a0eb3e32f6
Justin Lambert <justin.lambert@bigcommerce.com>
2013-11-15 13:46:16 +1100

    Working streaming tsv builder, BOOYAH

--- export_data.rb
+++ export_data.rb
@@ -13,11 +13,13 @@ class ExportData
   def products
     product_keys = @redis.keys("#{@namespace}:*")
     out = Enumerator.new do |yielder|
-      360.times do
+      4000.times do |i|
+        p i
         product_keys.each do |key|
           yielder << JSON.parse(@redis.get key)
         end
       end
+      @finished = true
     end
     out
   end
@@ -31,10 +33,17 @@ class ExportData
     require 'aws-sdk'
     s3 = AWS::S3.new
     obj = s3.buckets['test-feed-gen'].objects['out/file.tsv']
-    obj.write(estimated_content_length: 104857600) do |buffer, bytes|
-      buffer.write CSV.generate_line(["id", "description"], col_sep: "\t" )
-      products.each do |thing|
-        buffer.write CSV.generate_line([thing["id"], thing["description"]], col_sep: "\t")
+    obj.write(estimated_content_length: 10000000) do |buffer, bytes|
+      if (!@finished)
+        string = CSV.generate_line(["id", "description"], col_sep: "\t" )
+        buffer.write string
+        p string
+        products.each do |thing|
+          string = CSV.generate_line([thing["id"], thing["description"]], col_sep: "\t")
+          buffer.write string
+          # p string
+        end
+        p bytes
       end
     end
   end
